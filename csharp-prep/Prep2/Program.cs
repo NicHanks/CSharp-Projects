5625adbe482ef9ed0a9d13e4797709017cc9d70b
Nick <nikhanks@gmail.com>
2023-09-16 13:05:17 -0600

    trail

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is the percentage? ");
+        string percentageString = Console.ReadLine();
+        int percentageInt = int.Parse(percentageString);
+
+
+        if (percentageInt >= 90)
+        {
+            Console.WriteLine("Congrates! You got an A!");
+        }
+        else if (percentageInt >= 80)
+        {
+            Console.WriteLine("Congrates! You got an B!");
+
+        }
+        else if (percentageInt >= 70)
+        {
+            Console.WriteLine("Congrates! You got an C!");
+
+        }
+        else if (percentageInt >= 60)
+        {
+            Console.WriteLine("Congrates! You got an D!");
+
+        }
+        else if (percentageInt < 60)
+        {
+            Console.WriteLine("Congrates! You got an E!");
+
+        }
     }
 }
