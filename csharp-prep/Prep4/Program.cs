c84b57546a1fcf47a33687f23e6e730fb7312a5d
Nick <nikhanks@gmail.com>
2023-09-20 10:36:47 -0600

    first update

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic; //maybe I need this???
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = [2, 3, 23, 4];
+        List<string> words = ["yolo", "heyy", "hhi"];
+        List<int> nub;
+        nub = new List<int>{232, 454};
+        Console.WriteLine(nub);
+        numbers.Add(3);
+        
+
     }
 }
