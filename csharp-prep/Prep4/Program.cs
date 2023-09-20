893f744c9af9bb7e327b5402caf79654fbc3cf1f
Nick <nikhanks@gmail.com>
2023-09-20 13:37:25 -0600

    running into problems

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,15 +1,15 @@
 using System;
-using System.Collections.Generic; //maybe I need this???
+//using System.Collections.Generic; //maybe I need this???
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep4 World!");
-        List<int> numbers = [2, 3, 23, 4];
+        List<int> numbers = [3, 8, 9, 2];
         List<string> words = ["yolo", "heyy", "hhi"];
         List<int> nub;
-        nub = new List<int>{232, 454};
+        nub = new List<int>();
         Console.WriteLine(nub);
         numbers.Add(3);
         
