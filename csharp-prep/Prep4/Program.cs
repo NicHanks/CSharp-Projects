9d189ee8ba97ed7c6ddedf54036cee255d627b13
Nick <nikhanks@gmail.com>
2023-09-20 17:28:19 -0600

    see no probs

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,17 +6,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
-        List<int> numbers = [3, 8, 9, 2];
-        List<string> words = ["yolo", "heyy", "hhi"];
-        List<int> nub;
-        nub = new List<int>();
-        Console.WriteLine(nub);
-        numbers.Add(3);
+        // Console.WriteLine("Hello Prep4 World!");
+        // List<int> numbers = [3, 8, 9, 2];
+        // List<string> words = ["yolo", "heyy", "hhi"];
+        // List<int> nub;
+        // nub = new List<int>();
+        // Console.WriteLine(nub);
+        // numbers.Add(3);
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         int webResponse = 0;
-        List<int> numList = [];
+        List<int> numList;
+        numList = new List<int>();
+
         do
         {
             Console.Write("Enter number: ");
@@ -24,11 +26,13 @@ class Program
             numList.Add(webResponse);
 
         } while (webResponse != 0);
-        int sum = numList.Add();
-        int average = numList.Average();
-        int largestNum = numList.();
+
+        int sum = numList.Sum();
+        double average = numList.Average();
+        int largestNum = numList.Max();
+
         Console.Write($"The sum is: {sum}");
-        Console.Write($"The average is: {average}");
+        Console.Write($"The average is: {average}"); 
         Console.Write($"  The largest number is: {largestNum}");    
 
     }
