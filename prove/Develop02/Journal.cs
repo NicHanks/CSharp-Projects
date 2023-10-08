3c87cf10dacde70700a5263a2c5bd35bd082c85a
Nick <nikhanks@gmail.com>
2023-10-08 00:03:13 -0600

    Excel function updated

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -42,12 +42,29 @@ class Journal {
     //('exceeding the requirements')
     public void CreateCSV4Excel() {
         //Improve the process of saving and loading to save as a .csv file that could be opened in Excel (make sure to account for quotation marks and commas correctly in your content.
-        StreamWriter sw = new StreamWriter("excelEntries.csv");
-        sw.WriteLine("DateTime, Prompt, Input");
-        string[] stringedFile = new string;
-        foreach (string line in _entriesFileType) {
-            sw.WriteLine(line);
+        Console.WriteLine("Would you like to load a file before saving Entries to a new excel file? (y/n)");
+        string uI = Console.ReadLine();
+        if (uI == "y") {
+            LoadTheJournalFromAFile();
+        } else {
+            Console.WriteLine("What is the name of the Excel file you would like to save your journal to? ");
+            string input = Console.ReadLine();
+            using (StreamWriter sw2 = new StreamWriter(input))
+            {
+                sw2.WriteLine("DateTime, Prompt, UserInput");
+                string[] lines = System.IO.File.ReadAllLines("entries.txt");
+                foreach (string line in lines) {
+                    sw2.WriteLine(line);
+                }
+            }
         }
-        Console.WriteLine($"The name of the file is {excelFile}");
+        
+        // StreamWriter sw = new StreamWriter("excelEntries.csv");
+        // sw.WriteLine("DateTime, Prompt, Input");
+        // string[] stringedFile = new string;
+        // foreach (string line in _entriesFileType) {
+        //     sw.WriteLine(line);
+        // }
+        // Console.WriteLine($"The name of the file is {excelFile}");
     }
 }
